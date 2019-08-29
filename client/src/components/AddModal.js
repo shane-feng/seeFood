aac65f0e4ebf8c8bf1f74bd368de73e240c2c0d8
shane feng <shanevi29@gmail.com>
2019-08-28 20:53:18 -0700

    AddModal Component

---
+++ client/src/components/AddModal.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import '../App.css';
+import { Modal, ModalHeader, ModalBody, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+class AddModal extends Component {
+    // Ingredient name input
+    onChangeIngredient = e => {
+        this.setState({ ingredient: e.target.value });
+    };
+
+    // Ingredient weight input
+    onChangeWeight = e => {
+        this.setState({ weight: e.target.value.trim() });
+    };
+
+    // Ingredient expiration date input
+    onChangeExpires = e => {
+        this.setState({ expires: new Date(e.target.value) });
+    };
+
+    // Upload Ingredient Image
+    onChangeFile = e => {
+        const files = e.target.files;
+        const reader = new FileReader();
+
+        reader.addEventListener(
+            'load',
+            () => {
+                this.setState({
+                    image: reader.result
+                });
+                console.log(this.state.image);
+            },
+            false
+        );
+
+        reader.readAsDataURL(files[0]);
+    };
+
+    // Submit ingredient info
+    onSubmit = e => {
+        e.preventDefault();
+
+        const item = {
+            ingredient: this.state.ingredient,
+            weight: this.state.weight,
+            expires: this.state.expires,
+            image: this.state.image,
+            date: this.state.date
+        };
+
+        axios.post('http://localhost:5000/ingredients', item).catch(err => console.log(err));
+
+        setTimeout(() => {
+            alert(`${this.state.ingredient} has been successfully added!`);
+            window.location = '/';
+        }, 1000);
+    };
+
+    render() {
+        return (
+            <Modal isOpen={this.props.modal} toggle={this.props.addIngredient}>
+                <ModalHeader toggle={this.props.addIngredient}>Add Ingredient</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.onSubmit}>
+                        <FormGroup>
+                            <Label for="ingredient">Ingredient</Label>
+                            <Input
+                                type="text"
+                                name="ingredient"
+                                placeholder="Ingredient"
+                                onChange={this.onChangeIngredient}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="weight">Weight</Label>
+                            <Input type="text" name="weight" placeholder="Weight" onChange={this.onChangeWeight} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="expires">Expiration</Label>
+                            <Input type="date" name="date" placeholder="Date" onChange={this.onChangeExpires} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="image">Image</Label>
+                            <Input type="file" name="file" placeholder="Image URL" onChange={this.onChangeFile} />
+                        </FormGroup>
+                        <Button>Add Ingredient</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
+        );
+    }
+}
+
+export default AddModal;
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
 import '../App.css';
 
+import AddModal from './AddModal';
+
 class AppNavbar extends Component {
     state = {
-        isOpen: false
+        isOpen: false,
+        modal: false
     };
 
     // Mobile Menu Toggle
@@ -14,33 +17,37 @@ class AppNavbar extends Component {
         });
     };
 
+    // Add Ingredient Modal
+    addIngredient = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    };
+
     render() {
         return (
-            <Navbar dark expand="md">
-                <Container className="navBar">
-                    <NavbarBrand href="/">SeeFood</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/about">About</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/">Ingredients</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/add">Add</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/logout">Logout</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/">Login</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar>
+            <div>
+                <Navbar dark expand="md">
+                    <Container className="navBar">
+                        <NavbarBrand href="/">SeeFood</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="/about">About</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/">My Ingredients</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink onClick={this.addIngredient}>Add Ingredients</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+                <AddModal modal={this.state.modal} addIngredient={this.addIngredient}></AddModal>
+            </div>
         );
     }
 }
