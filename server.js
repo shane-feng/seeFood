a73a7177ccd5507239f7cd2f4ae8f5309d1a8b7a
shane feng <shanevi29@gmail.com>
2019-08-30 12:27:33 -0700

    deploy fix v3

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ connection.once('open', () => {
 // Static Files When In Production
 if (process.env.NODE_ENV === 'production') {
     // Set Static Folder
-    app.use('/', express.static('client/build'));
+    app.use('https://seefood2019.herokuapp.com/', express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
